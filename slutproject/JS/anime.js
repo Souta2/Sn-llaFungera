4f89cc24a3f769f794e10f2737960b57435c59c2
Souta2 <95079661+Souta2@users.noreply.github.com>
2022-03-28 17:50:22 +0200

    Add files via upload

--- slutproject/JS/anime.js
+++ slutproject/JS/anime.js
@@ -1,27 +1,4 @@
-function lightOn() {
-    if (document.body.style.background !== "white") {
-                document.body.style.background = "white";
-                document.getElementById("colorChange1").style.color = "black";
-                document.getElementById("colorChange2").style.color = "black";
-                document.getElementById("colorChange3").style.color = "black";
-                document.getElementById("colorChange4").style.color = "black";
-                document.getElementById("colorChange5").style.color = "black";
-                document.getElementById("colorChange6").style.color = "black";
-                document.getElementById("colorChange7").style.color = "black";
-                document.getElementById("colorChange8").style.color = "black";
-            }
-            else {
-                document.body.style.background = "rgb(38, 38, 38)";
-                document.getElementById("colorChange1").style.color = "white";
-                document.getElementById("colorChange2").style.color = "white";
-                document.getElementById("colorChange3").style.color = "white";
-                document.getElementById("colorChange4").style.color = "white";
-                document.getElementById("colorChange5").style.color = "white";
-                document.getElementById("colorChange6").style.color = "white";
-                document.getElementById("colorChange7").style.color = "white";
-                document.getElementById("colorChange8").style.color = "white";
-            }
-}
+
 
         /* When the user clicks on the button, 
         toggle between hiding and showing the dropdown content */
